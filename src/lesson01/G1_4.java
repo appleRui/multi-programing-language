5ea28daf8d59ed9b8aa8e96385757165fba61a7b
appleRui <apple31581@gmail.com>
2022-09-21 12:25:25 +0900

    0921 Lesson01

---
+++ src/lesson01/G1_4.java
@@ -0,0 +1,25 @@
+package lesson01;
+
+public class G1_4 {
+  public static void main(String[] args) {
+    Integer[] list = {5,2,3,9,1};
+    for (Integer v: sort(list)) {
+      System.out.print(v);
+    }
+  }
+
+  public static Integer[] sort (Integer[] list) {
+    for (int i = 0; i<list.length-1; i++){
+      for (int j = 0; j < list.length-1-i; j++){
+        Integer v1 = list[j];
+        Integer v2 = list[j+1];
+        if(v1.compareTo(v2) > 0) {
+          Integer tmp = list[j];
+          list[j] = list[j+1];
+          list[j+1] = tmp;
+        }
+      }
+    }
+    return list;
+  }
+}
